3dae6e0df23d98be51538dc085f1953f1ada2f66
Mizan Choudhury <mizan_98@hotmail.co.uk>
2021-01-12 15:15:50 +0000

    adding form

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const Register = () => {
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const formHandler = (event) => {
+
+        console.log(name)
+        console.log(email)
+        console.log(password)
+        event.preventDefault();
+
+    }
+
     return (
         <div>
             Register page
+            <form onSubmit={formHandler}>
+                <label>name</label>
+                <input type="text" name="userName" onChange={(e) => setName(e.target.value)}/>
+                <label>name</label>
+                <input type="email" name="userEmail" onChange={(e) => setEmail(e.target.value)}/>
+                <label>password</label>
+                <input type="password" name="userPassword" onChange={(e) => setPassword(e.target.value)}/>
+                <button type="submit">submit</button>
+            </form>
         </div>
     )
 }
